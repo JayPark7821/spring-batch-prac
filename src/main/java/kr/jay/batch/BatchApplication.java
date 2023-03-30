0b2cbce91361d64df9062bafc962ac6fc51b3101
jaypark <pjhyun1990@gmail.com>
2023-03-30 18:14:30 +0900

    spring-batch part 1

---
+++ src/main/java/kr/jay/batch/BatchApplication.java
@@ -0,0 +1,15 @@
+package kr.jay.batch;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableBatchProcessing
+@SpringBootApplication
+public class BatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/person.sql
@@ -0,0 +1,12 @@
+-- create table person(
+--     id bigint primary key auto_increment,
+--     name varchar(255),
+--     age varchar(255),
+--     address varchar(255)
+-- );
+
+insert into person (name, age, address) values
+('박재현','32','인천'),
+('홍길동','30','서울'),
+('김철수','28','부산'),
+('이영희','26','대구');
